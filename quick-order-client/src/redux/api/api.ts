7ad9bb46baf527ece6f5fa4b875df591609fecd2
Nabiur siddique <nabiursiddique01@gmail.com>
2024-09-05 13:21:25 +0600

    client side is now connected with server

--- quick-order-client/src/redux/api/api.ts
+++ quick-order-client/src/redux/api/api.ts
@@ -1,24 +1,24 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const baseApi = createApi({
-  reducerPath: "baseApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3000/api/v1" }),
-  tagTypes: ["products", "verify"],
+  reducerPath: 'baseApi',
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000/api/v1' }),
+  tagTypes: ['products', 'verify'],
   endpoints: (builder) => ({
     getProducts: builder.query({
       query: () => ({
-        method: "GET",
-        url: "/product",
+        method: 'GET',
+        url: '/product',
       }),
-      providesTags: ["products"],
+      providesTags: ['products'],
     }),
     creteOrder: builder.mutation({
       query: (data) => {
         return {
-          method: "POST",
+          method: 'POST',
           url: `/order/create`,
           body: data,
         };
-      }
+      },
     }),
   }),
 });
