b8e538ca90847181497c475483867f5896ffb05f
Nabiur siddique <nabiursiddique01@gmail.com>
2024-09-05 13:20:30 +0600

    server is now ready to use

--- quick-order-server/src/app.ts
+++ quick-order-server/src/app.ts
@@ -4,7 +4,7 @@ import connectDB from './app/config/db';
 import { seedProducts } from './app/config/seed';
 import { orderRoutes } from './app/modules/order/order.routes';
 import { productRoutes } from './app/modules/product/product.routes';
-import cors from "cors";
+import cors from 'cors';
 
 const app = express();
 
@@ -14,17 +14,17 @@ app.use(cors());
 app.use('/api/v1/product', productRoutes);
 app.use('/api/v1/order', orderRoutes);
 
-app.get("/", (req: Request, res: Response) => {
-    res.send({
-        message: "Server is running!",
-        status: "success",
-        timestamp: new Date().toISOString(),
-        environment: process.env.NODE_ENV || "development",
-        version: "v1.0.0"
-    });
+app.get('/', (req: Request, res: Response) => {
+  res.send({
+    message: 'Server is running!',
+    status: 'success',
+    timestamp: new Date().toISOString(),
+    environment: process.env.NODE_ENV || 'development',
+    version: 'v1.0.0',
+  });
 });
 
 connectDB();
-//seedProducts();
+seedProducts();
 
 export default app;
