32de4880985ffd2330b291acc246f8ec50b016d0
Nabiur siddique <nabiursiddique01@gmail.com>
2024-09-05 14:05:47 +0600

    payment page now showing directly in frontend

--- quick-order-client/src/pages/CheckOutPage.tsx
+++ quick-order-client/src/pages/CheckOutPage.tsx
@@ -1,15 +1,15 @@
-import React, { useState } from "react";
-import { useAppSelector } from "../redux/hooks";
-import { useCreteOrderMutation } from "../redux/api/api";
+import React, { useState } from 'react';
+import { useAppSelector } from '../redux/hooks';
+import { useCreteOrderMutation } from '../redux/api/api';
 
 export default function CheckOutPage() {
-  const [createOrder] = useCreteOrderMutation()
+  const [createOrder] = useCreteOrderMutation();
   // State for user input
   const [user, setUser] = useState({
-    name: "Fahim Ahammed",
-    email: "fahim@ph.com",
-    phone: "0123456789",
-    address: "Dhaka, Bangladesh",
+    name: 'Nabiur Siddique',
+    email: 'nabiur@siddique.com',
+    phone: '0123456789',
+    address: 'Dhaka, Bangladesh',
   });
 
   // Replace this with actual selector from your Redux store // Replace with your Redux selector
@@ -34,109 +34,110 @@ export default function CheckOutPage() {
     try {
       const res = await createOrder(data).unwrap();
       if (res.success) {
-        console.log(res)
+        console.log(res);
+        window.location.href = res.data.payment_url;
       } else {
         console.error('Order creation failed:', res.message);
       }
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
   };
 
   return (
-    <div className="max-w-4xl mx-auto p-6 bg-white rounded-lg shadow-lg">
-      <h2 className="text-3xl font-bold mb-6">Checkout</h2>
+    <div className='max-w-4xl mx-auto p-6 bg-white rounded-lg shadow-lg'>
+      <h2 className='text-3xl font-bold mb-6'>Checkout</h2>
       <form onSubmit={handleSubmit}>
-        <div className="mb-8 border p-5 rounded">
-          <h3 className="text-xl font-semibold mb-4">User Information</h3>
-          <div className="grid grid-cols-1 gap-6 sm:grid-cols-2">
+        <div className='mb-8 border p-5 rounded'>
+          <h3 className='text-xl font-semibold mb-4'>User Information</h3>
+          <div className='grid grid-cols-1 gap-6 sm:grid-cols-2'>
             <div>
-              <label className="block text-sm font-medium text-gray-700 text-left">
+              <label className='block text-sm font-medium text-gray-700 text-left'>
                 Name
               </label>
               <input
-                type="text"
-                name="name"
+                type='text'
+                name='name'
                 value={user.name}
                 onChange={handleChange}
                 required
-                className="mt-1 block w-full p-2 border border-gray-300 rounded-lg shadow-sm focus:ring-green-500 focus:border-green-500"
+                className='mt-1 block w-full p-2 border border-gray-300 rounded-lg shadow-sm focus:ring-green-500 focus:border-green-500'
               />
             </div>
             <div>
-              <label className="block text-sm font-medium text-gray-700 text-left">
+              <label className='block text-sm font-medium text-gray-700 text-left'>
                 Email
               </label>
               <input
-                type="email"
-                name="email"
+                type='email'
+                name='email'
                 value={user.email}
                 onChange={handleChange}
                 required
-                className="mt-1 block w-full p-2 border border-gray-300 rounded-lg shadow-sm focus:ring-green-500 focus:border-green-500"
+                className='mt-1 block w-full p-2 border border-gray-300 rounded-lg shadow-sm focus:ring-green-500 focus:border-green-500'
               />
             </div>
             <div>
-              <label className="block text-sm font-medium text-gray-700 text-left">
+              <label className='block text-sm font-medium text-gray-700 text-left'>
                 Phone
               </label>
               <input
-                type="text"
-                name="phone"
+                type='text'
+                name='phone'
                 value={user.phone}
                 onChange={handleChange}
                 required
-                className="mt-1 block w-full p-2 border border-gray-300 rounded-lg shadow-sm focus:ring-green-500 focus:border-green-500"
+                className='mt-1 block w-full p-2 border border-gray-300 rounded-lg shadow-sm focus:ring-green-500 focus:border-green-500'
               />
             </div>
             <div>
-              <label className="block text-sm font-medium text-gray-700 text-left">
+              <label className='block text-sm font-medium text-gray-700 text-left'>
                 Address
               </label>
               <input
-                type="text"
-                name="address"
+                type='text'
+                name='address'
                 value={user.address}
                 onChange={handleChange}
                 required
-                className="mt-1 block w-full p-2 border border-gray-300 rounded-lg shadow-sm focus:ring-green-500 focus:border-green-500"
+                className='mt-1 block w-full p-2 border border-gray-300 rounded-lg shadow-sm focus:ring-green-500 focus:border-green-500'
               />
             </div>
           </div>
         </div>
 
-        <div className="mb-8">
-          <h3 className="text-xl font-semibold mb-4">Order Summary</h3>
-          <table className="min-w-full bg-white border border-gray-300">
+        <div className='mb-8'>
+          <h3 className='text-xl font-semibold mb-4'>Order Summary</h3>
+          <table className='min-w-full bg-white border border-gray-300'>
             <thead>
-              <tr className="bg-gray-200">
-                <th className="text-left py-3 px-4 font-semibold text-sm">
+              <tr className='bg-gray-200'>
+                <th className='text-left py-3 px-4 font-semibold text-sm'>
                   Product
                 </th>
-                <th className="text-left py-3 px-4 font-semibold text-sm">
+                <th className='text-left py-3 px-4 font-semibold text-sm'>
                   Quantity
                 </th>
-                <th className="text-left py-3 px-4 font-semibold text-sm">
+                <th className='text-left py-3 px-4 font-semibold text-sm'>
                   Price
                 </th>
               </tr>
             </thead>
             <tbody>
               {cartItems.map((item) => (
-                <tr key={item._id} className="border-b">
-                  <td className="py-3 px-4">{item.name}</td>
-                  <td className="py-3 px-4">{item.quantity}</td>
-                  <td className="py-3 px-4">${item.price}</td>
+                <tr key={item._id} className='border-b'>
+                  <td className='py-3 px-4'>{item.name}</td>
+                  <td className='py-3 px-4'>{item.quantity}</td>
+                  <td className='py-3 px-4'>${item.price}</td>
                 </tr>
               ))}
             </tbody>
           </table>
         </div>
 
-        <div className="flex justify-end">
+        <div className='flex justify-end'>
           <button
-            type="submit"
-            className="bg-green-600 text-white font-semibold py-2 px-6 rounded-lg hover:bg-green-700 transition duration-300"
+            type='submit'
+            className='bg-green-600 text-white font-semibold py-2 px-6 rounded-lg hover:bg-green-700 transition duration-300'
           >
             Proceed to Payment
           </button>
